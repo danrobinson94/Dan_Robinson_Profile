2025803f50b8803078e8887aa3ae41bb32b8880d
danfragmints <dan94.robinson@gmail.com>
2022-09-23 15:38:42 -0600

    added iphone 13

--- src/components/BootstrapCarousel.js
+++ src/components/BootstrapCarousel.js
@@ -3,6 +3,7 @@ import { Carousel } from "react-bootstrap";
 import ReactPlayer from "react-player";
 import EtherealSite from "../media/ethereal_site.png";
 import Wordle from "../media/gordle.png";
+import iPhone from "../media/iphone.png";
 
 export default function BootstrapCarousel() {
   return (
@@ -10,25 +11,31 @@ export default function BootstrapCarousel() {
       <Carousel>
         <Carousel.Item>
           <div class="row">
-            <ReactPlayer
-              class="proj-media"
-              url="https://player.vimeo.com/video/714608664?h=98866b7d23"
-              height="420px"
-              width="750px"
-            />
+            <img class="project-image" src={iPhone} />
           </div>
           <div>
             <Carousel.Caption class="row project-desc">
-              <h3 class="project-title">Rich Ape Bot Club</h3>
+              <h3 class="project-title">iPhone 13 Pro</h3>
               <p class="project-data no-links">
-                Built in Node with Puppeteer. Logs in to Metamask Chrome
-                extension. Creates offers on NFT's based on user's collection
-                and property filtering inputs, and keeps track of competing
-                offers to automatically outbid them if within user parameters.
+                iPhone 13 Pro marketing website using GreenSock. Capstone
+                project from a web development course, with a few of my own
+                animations. Banner zoom-in video created in Blender.
               </p>
-              <a href="https://github.com/danrobinson94/rich_ape_bot_club">
-                View the repo
-              </a>
+              <div class="image-link-section iphone-links">
+                <a
+                  class="image-link-left"
+                  href="https://iphone-13-website.vercel.app/"
+                >
+                  View the repo
+                </a>
+
+                <a
+                  class="image-link-right"
+                  href="https://github.com/danrobinson94/iPhone_13_Website"
+                >
+                  Website
+                </a>
+              </div>
               {/* <div class="bot-link-spacer" /> */}
             </Carousel.Caption>
           </div>
@@ -76,6 +83,31 @@ export default function BootstrapCarousel() {
             </Carousel.Caption>
           </div>
         </Carousel.Item>
+        <Carousel.Item>
+          <div class="row">
+            <ReactPlayer
+              class="proj-media"
+              url="https://player.vimeo.com/video/714608664?h=98866b7d23"
+              height="420px"
+              width="750px"
+            />
+          </div>
+          <div>
+            <Carousel.Caption class="row project-desc">
+              <h3 class="project-title">Rich Ape Bot Club</h3>
+              <p class="project-data no-links">
+                Built in Node with Puppeteer. Logs in to Metamask Chrome
+                extension. Creates offers on NFT's based on user's collection
+                and property filtering inputs, and keeps track of competing
+                offers to automatically outbid them if within user parameters.
+              </p>
+              <a href="https://github.com/danrobinson94/rich_ape_bot_club">
+                View the repo
+              </a>
+              {/* <div class="bot-link-spacer" /> */}
+            </Carousel.Caption>
+          </div>
+        </Carousel.Item>
         <Carousel.Item class="carousel-things">
           <div class="row">
             <img class="project-image" src={EtherealSite} />
@@ -92,13 +124,16 @@ export default function BootstrapCarousel() {
               </p>
 
               <div class="image-link-section">
-                <a class="image-link-left" href="https://etherealartnft.com/">
-                  Ethereal Art Website
+                <a
+                  class="image-link-left"
+                  href="https://twitter.com/etherealartnft"
+                >
+                  Ethereal Art Twitter
                 </a>
 
                 <a
                   class="image-link-right"
-                  href="https://opensea.io/collection/ethereal-art-ladowska"
+                  href="https://opensea.io/collection/etherealartorigins"
                 >
                   Ethereal Art on Opensea
                 </a>
